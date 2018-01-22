23482223a616c68c3eaefbb326f7133e0e655939
Juliana Davila R <judaro13@gmail.com>
2018-01-22 09:41:09 -0500

    adding errors

--- main.go
+++ main.go
@@ -2,22 +2,45 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"os"
+
+	"github.com/streadway/amqp"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
 
 var (
 	ErrEnv        = errors.New("not found RABBIT_PATH  environment variable")
 	ErrEnvChannel = errors.New("not found RABBIT_CHANNEL  environment variable")
+	ErrConnect    = errors.New("failed to connect to RabbitMQ")
+	ErrChannel    = errors.New("failed to open a channel")
+	ErrQueue      = errors.New("failed to declare a queue")
+	ErrPublish    = errors.New("failed to publish a message")
 )
 
 func main() {
 	rabbit_url := os.Getenv("RABBIT_PATH")
 	rabbit_ch := os.Getenv("RABBIT_CHANNEL")
 	if len(rabbit_url) == 0 {
-		return ErrEnv
+		failOnError(ErrEnv, ErrEnv.Error())
 	}
 	if len(rabbit_ch) == 0 {
-		return ErrEnvChannel
+		failOnError(ErrEnvChannel, ErrEnvChannel.Error())
 	}
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, ErrConnect.Error())
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, ErrChannel.Error())
+	defer ch.Close()
 
 }
