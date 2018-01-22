01518de764357b52483b4385928d128378a5d616
Juliana Davila R <judaro13@gmail.com>
2018-01-22 09:35:42 -0500

    adding validations

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"errors"
+	"os"
+)
+
+var (
+	ErrEnv        = errors.New("not found RABBIT_PATH  environment variable")
+	ErrEnvChannel = errors.New("not found RABBIT_CHANNEL  environment variable")
+)
+
 func main() {
+	rabbit_url := os.Getenv("RABBIT_PATH")
+	rabbit_ch := os.Getenv("RABBIT_CHANNEL")
+	if len(rabbit_url) == 0 {
+		return ErrEnv
+	}
+	if len(rabbit_ch) == 0 {
+		return ErrEnvChannel
+	}
 
 }
